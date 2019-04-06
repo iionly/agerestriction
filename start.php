394d3e014979f77c5f6501fd98bab9df97fdc5ab
iionly <iionly@gmx.de>
2019-04-06 16:00:14 +0200

    Updated to version 3.0.0; changes see CHANGES.txt

--- start.php
+++ start.php
@@ -11,10 +11,10 @@ function agerestriction_init() {
 	elgg_extend_view('register/extend', 'agerestriction/register', 1000);
 
 	//block user registration if they don't check the box
-	elgg_register_plugin_hook_handler('action', 'register', 'agerestriction_register_hook');
+	elgg_register_plugin_hook_handler('action:validate', 'register', 'agerestriction_register_hook');
 }
 
-function agerestriction_register_hook() {
+function agerestriction_register_hook(\Elgg\Hook $hook) {
 	elgg_make_sticky_form('register');
 
 	$agevalid = (int) get_input('agevalid', 0);
